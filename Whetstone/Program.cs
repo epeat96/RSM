0194fb397e531d67b573594aa5e4ed373676a10b
Cristobal Barreto <44922559+epeat96@users.noreply.github.com>
2026-04-29 11:28:25 -0300

    Add initial project files and implement main program logic

---
+++ Whetstone/Program.cs
@@ -0,0 +1,10 @@
+﻿using Whetstone.LeetCode.Problem2;
+
+var solution = new Solution();
+
+var l1 = new ListNode(9,
+    new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+var l2 = new ListNode(9,
+    new ListNode(9, new ListNode(9, new ListNode(9))));
+
+solution.AddTwoNumbers(l1, l2);
